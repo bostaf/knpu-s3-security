54aeb946915f015d14bc64182c3c0a226e91a209
Grzegorz Szaliński <grzegorz.szalinski@gmail.com>
2016-06-28 19:58:45 +0200

    Add controler and view for login

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * User: bostaf
+ * Date: 28.06.16
+ * Time: 19:21
+ */
+
+namespace AppBundle\Entity;
+
+
+use Symfony\Component\Security\Core\Role\Role;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", unique=true)
+     */
+    private $email;
+
+    public function getUsername()
+    {
+        return $this->email;
+    }
+
+    public function getRoles()
+    {
+        return ['ROLE_USER'];
+    }
+
+    public function getPassword()
+    {
+    }
+
+    public function getSalt()
+    {
+    }
+
+    public function eraseCredentials()
+    {
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+}
