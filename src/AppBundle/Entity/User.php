8774eac2535050235aaa08d1650bc2092fa3042e
Grzegorz Szaliński <grzegorz.szalinski@gmail.com>
2016-07-03 20:05:39 +0200

    Add user roles

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -37,6 +37,11 @@ class User implements UserInterface
 
     private $plainPassword;
 
+    /**
+     * @ORM\Column(type="json_array")
+     */
+    private $roles = [];
+
     public function getUsername()
     {
         return $this->email;
@@ -44,7 +49,12 @@ class User implements UserInterface
 
     public function getRoles()
     {
-        return ['ROLE_USER'];
+        $roles = $this->roles;
+        if (!in_array('ROLE_USER', $roles)) {
+            $roles[] = 'ROLE_USER';
+        }
+
+        return $roles;
     }
 
     public function getPassword()
@@ -92,5 +102,10 @@ class User implements UserInterface
     {
         $this->plainPassword = $plainPassword;
         $this->password = null;
+    }
+
+    public function setRoles($roles)
+    {
+        $this->roles = $roles;
     }
 }
